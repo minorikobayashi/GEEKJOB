8c4da0646d15b628c0713e73f97b080e27421378
DESKTOP-0FUVAON\小林誠典 <kminori.5173@gmail.com>
2018-07-01 10:34:37 +0900

    カリキュラムjava　課題引数、戻り値２を提出します。

---
+++ CampChallenge_java/011.メソッド/method7.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package method;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 小林誠典
+ */
+@WebServlet(name = "method7", urlPatterns = {"/method7"})
+public class method7 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    String[] userProfile(String a){
+         String[] one={"1","技育太郎","東京",null,"プログラマー"};
+        String[] two={"2","技育花子",null,"女","システムエンジニア"};
+        String[] three={"3","技育三郎","大阪","男",null};
+        
+        int i =Integer.valueOf(a);
+        if(a.equals(one[0])){return one;}
+        else if(a.equals(two[0])){return two;}
+        else if(a.equals(three[0])){return three;}
+        else{return null;}
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet method7</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            for(int i=0; i<userProfile("1").length;i++){
+                
+               if(i>0){
+                   if(userProfile("1")[i]==null){continue;}
+                   out.print(userProfile("1")[i]);
+               
+               }
+            }
+            out.println("<h1>Servlet method7 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
