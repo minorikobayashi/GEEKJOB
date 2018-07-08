112b0ad0c6a661eca8ad39eb5c20e270168fa5f1
DESKTOP-0FUVAON\小林誠典 <kminori.5173@gmail.com>
2018-07-08 22:00:12 +0900

    カリキュラムjava　課題　文字数の取得を提出します。

---
+++ CampChallenge_java/015.標準クラス/challenge6.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nojiretu;
+
+/**
+ *
+ * @author 小林誠典
+ */
+public class challenge6 {
+    public static void main(String[]args){
+        String mail="kminori.5173@gmail.com";
+        System.out.println(mail.substring(12,22).length());
+    }
+}
