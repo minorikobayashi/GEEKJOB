6dc27a3a8583b0d88c82b8e0cbc3d89acd3b8b71
DESKTOP-0FUVAON\小林誠典 <kminori.5173@gmail.com>
2018-07-08 21:59:20 +0900

    カリキュラムjava　課題バイト数と文字数の取得を提出します。

---
+++ CampChallenge_java/015.標準クラス/challenge5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nojiretu;
+
+/**
+ *
+ * @author 小林誠典
+ */
+public class challenge5 {
+    public static void main(String[]args){
+        String name = "小林誠典";
+        System.out.println(name.length());
+        System.out.println(name.getBytes().length);
+    }
+    
+}
