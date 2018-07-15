975656a76fb89ccb169b97053ef5cbd0ca550cd8
DESKTOP-0FUVAON\小林誠典 <kminori.5173@gmail.com>
2018-07-15 13:11:48 +0900

    DB操作　課題　エラーハンドリングを含んだ接続の確立を提出します。

---
+++ CampChallenge_java/018.DB操作/jdbc.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ *
+ * @author 小林誠典
+ */
+@WebServlet(urlPatterns = {"/jdbc"})
+public class jdbc extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    
+   
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet jdbc</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            Connection db_con = null;
+            PreparedStatement db_st = null;
+            ResultSet db_date= null;
+//            int x = 100 / 0;
+            
+            Class.forName("con.myspl.jdbc.Driver").newInstance();
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+            db_st=db_con.prepareStatement("select*from sample where age=?");
+            db_st.setInt(1,33);
+            
+            db_date = db_st.executeQuery();
+            while(db_date.next()){
+                out.print("名前"+db_date.getString("name")+"<br>");
+            }
+            
+            db_date.close();
+            db_st.close();
+            db_con.close();
+
+            out.println("</body>");
+            out.println("</html>");
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
