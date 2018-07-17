728bb2299fa2deeccbbd7de046a7b27092c6b50a
DESKTOP-0FUVAON\小林誠典 <kminori.5173@gmail.com>
2018-07-17 14:54:32 +0900

    DB操作　課題　複合検索を提出します。

---
+++ CampChallenge_java/018.DB操作/jdbc12.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ *
+ * @author 小林誠典
+ */
+@WebServlet(urlPatterns = {"/jdbc12"})
+public class jdbc12 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet jdbc12</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            request.setCharacterEncoding("UTF-8");
+            String name=request.getParameter("name");
+            int age=Integer.parseInt(request.getParameter("age"));
+            String birthday=request.getParameter("birthday");
+            
+            Connection con =null;
+            PreparedStatement st =null;
+            ResultSet data=null;
+            
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GEEKJOB_kobayashi?useUnicode=true&useJDBCCompliantTimezoneShift="
+                    + "true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                    "KOBAYASHI","password5173");
+            
+            st=con.prepareStatement("select * from profiles where name=? or age=? or birthday=?");
+            st.setString(1,name);
+            st.setInt(2,age);
+            st.setString(3, birthday);
+            
+            data=st.executeQuery();
+            
+            while(data.next()){
+                out.print("profileID:"+data.getInt("profileID")+"<br>");
+                out.print("name:"+data.getString("name")+"<br>");
+                out.print("tel:"+data.getString("tel")+"<br>");
+                out.print("age:"+data.getInt("age")+"<br>");
+                out.print("birthday:"+data.getString("birthday")+"<br>");
+            }
+            
+            data.close();
+            st.close();
+            con.close();
+            
+            out.println("</body>");
+            out.println("</html>");
+        }catch(Exception ex){
+            System.out.print(ex.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
